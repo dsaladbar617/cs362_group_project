4677f102f0c04fe68e199ef21680a6c6d9058d7f
Daniel Salazar <98618929+dsaladbar617@users.noreply.github.com>
2025-02-18 15:42:31 -0600

    Update tests.py

--- tests.py
+++ tests.py
@@ -6,11 +6,11 @@ class TestCase(unittest.TestCase):
     def test1(self):
         expected = "Hello World"
         self.assertEqual(task.my_func(), expected)
-    
+
     def test2(self):
         expected = "Hola World"
         self.assertEqual(task.my_func(), expected)
 
 
 if __name__ == '__main__':
-  unittest.main()
+    unittest.main()
